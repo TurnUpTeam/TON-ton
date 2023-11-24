9aaa8ae4eb9ea00afb499fb9206923d17a1ffb13
Francesco Sullo <sullof@gmail.com>
2023-11-23 16:05:41 -0800

    All tests pass

--- test/Integration.spec.ts
+++ test/Integration.spec.ts
@@ -2,7 +2,7 @@ import '@ton-community/test-utils';
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton-community/sandbox';
 import { Address, beginCell, toNano } from 'ton-core';
 
-import { Shares, NewKey } from '../wrappers/Shares';
+import { Shares, NewKey, TradeKey } from '../wrappers/Shares';
 import { SharesKey } from '../wrappers/SharesKey';
 import { SharesWallet } from '../wrappers/SharesWallet';
 
@@ -12,6 +12,8 @@ describe('Integration', () => {
     let blockchain: Blockchain;
 
     let newKeyMsg: NewKey;
+    let tradeKeyMsg: TradeKey;
+
     // contracts
     let wallet: SandboxContract<SharesWallet>;
     let key: SandboxContract<SharesKey>;
@@ -51,17 +53,11 @@ describe('Integration', () => {
         });
     });
 
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and shares are ready to use
-    });
-
-    it("should expect the right parameters", async()=>{
-        expect(await shares.getGetFeeDestination()).toEqualAddress(deployer.address);
-        expect(await shares.getGetFeePercentage()).toEqual(5n);
-        expect(await shares.getGetSubjectFeePercentage()).toEqual(5n);
-
-    });
+    // it('should deploy', async () => {
+    //     expect(await shares.getGetFeeDestination()).toEqualAddress(deployer.address);
+    //     expect(await shares.getGetFeePercentage()).toEqual(5n);
+    //     expect(await shares.getGetSubjectFeePercentage()).toEqual(5n);
+    // });
 
     it("should create a first key and the relative holder", async()=>{
 
@@ -165,6 +161,214 @@ describe('Integration', () => {
 
         const balanceAfter = await subject2.getBalance();
         expect(balanceBefore - balanceAfter).toBeLessThan(100000000n);
+
+    })
+
+    it("should create allow holder to buy a key", async()=>{
+
+        let price = await shares.getGetPrice(0n, 3n);
+        let protocolFeePercentage = await shares.getGetFeePercentage();
+        let subjectFeePercentage = await shares.getGetSubjectFeePercentage();
+
+        const gasConsumption = await shares.getGetGasConsumption();
+
+        // @ts-ignore
+        let protocolFee = price * protocolFeePercentage / 100n;
+        // @ts-ignore
+        let subjectFee = price * subjectFeePercentage / 100n;
+
+        subject = await blockchain.treasury('subject');
+
+        newKeyMsg = {
+            $$type: 'NewKey',
+            subject: subject.address,
+            initialSupply: 3n,
+        };
+
+        await shares.send(
+            subject.getSender(),
+            {
+                value: price + protocolFee + subjectFee + gasConsumption,
+            },
+            newKeyMsg
+        );
+
+        holder = await blockchain.treasury('holder');
+
+        // buying 5 keys
+        tradeKeyMsg = {
+            $$type: 'TradeKey',
+            subject: subject.address,
+            supply: 3n,
+            holder: holder.address,
+            balance: 0n,
+            amount: 5n,
+            increment: true
+        };
+
+        price = await shares.getGetPrice(3n, 5n);
+        protocolFeePercentage = await shares.getGetFeePercentage();
+        subjectFeePercentage = await shares.getGetSubjectFeePercentage();
+
+        // @ts-ignore
+        protocolFee = price * protocolFeePercentage / 100n;
+        // @ts-ignore
+        subjectFee = price * subjectFeePercentage / 100n;
+
+        const result = await shares.send(
+            holder.getSender(),
+            {
+                value: price + protocolFee + subjectFee + gasConsumption,
+            },
+            tradeKeyMsg
+        );
+
+        expect(result.transactions).toHaveTransaction({
+            from: holder.address,
+            to: shares.address,
+            success: true
+        })
+
+        const keyAddress = await shares.getGetKeyAddress(subject.address);
+        expect(result.transactions).toHaveTransaction({
+            from: shares.address,
+            to: keyAddress,
+            success: true
+        })
+
+        // key supply
+
+        const keyContract = blockchain.openContract(await SharesKey.fromAddress(keyAddress));
+        const keySupply = await keyContract.getSupply();
+        expect(keySupply).toEqual(8n);
+
+        // wallet balance
+        const walletAddress = await shares.getGetWalletAddress(holder.address, subject.address);
+        expect(result.transactions).toHaveTransaction({
+            from: shares.address,
+            to: walletAddress,
+            success: true
+        })
+
+        const walletContract = blockchain.openContract(await SharesWallet.fromAddress(walletAddress));
+
+        const walletBalance = await walletContract.getBalance();
+        expect(walletBalance).toEqual(5n);
+
+    })
+
+
+    it("should create allow holder to sell a key", async()=>{
+
+        let price = await shares.getGetPrice(0n, 3n);
+        let protocolFeePercentage = await shares.getGetFeePercentage();
+        let subjectFeePercentage = await shares.getGetSubjectFeePercentage();
+
+        const gasConsumption = await shares.getGetGasConsumption();
+
+        // @ts-ignore
+        let protocolFee = price * protocolFeePercentage / 100n;
+        // @ts-ignore
+        let subjectFee = price * subjectFeePercentage / 100n;
+
+        subject = await blockchain.treasury('subject');
+
+        newKeyMsg = {
+            $$type: 'NewKey',
+            subject: subject.address,
+            initialSupply: 3n,
+        };
+
+        await shares.send(
+            subject.getSender(),
+            {
+                value: price + protocolFee + subjectFee + gasConsumption,
+            },
+            newKeyMsg
+        );
+
+        holder = await blockchain.treasury('holder');
+
+        // buying 5 keys
+        tradeKeyMsg = {
+            $$type: 'TradeKey',
+            subject: subject.address,
+            supply: 3n,
+            holder: holder.address,
+            balance: 0n,
+            amount: 5n,
+            increment: true
+        };
+
+        price = await shares.getGetPrice(3n, 5n);
+        protocolFeePercentage = await shares.getGetFeePercentage();
+        subjectFeePercentage = await shares.getGetSubjectFeePercentage();
+
+        // @ts-ignore
+        protocolFee = price * protocolFeePercentage / 100n;
+        // @ts-ignore
+        subjectFee = price * subjectFeePercentage / 100n;
+
+        await shares.send(
+            holder.getSender(),
+            {
+                value: price + protocolFee + subjectFee + gasConsumption,
+            },
+            tradeKeyMsg
+        );
+
+        // selling 4 keys
+        tradeKeyMsg = {
+            $$type: 'TradeKey',
+            subject: subject.address,
+            supply: 8n,
+            holder: holder.address,
+            balance: 5n,
+            amount: 4n,
+            increment: false
+        };
+
+        // console.log("\n\n\n\n\n\n\n\n\n\n\n");
+
+        const result = await shares.send(
+            holder.getSender(),
+            {
+                value: gasConsumption,
+            },
+            tradeKeyMsg
+        );
+
+        expect(result.transactions).toHaveTransaction({
+            from: holder.address,
+            to: shares.address,
+            success: true
+        })
+
+        const keyAddress = await shares.getGetKeyAddress(subject.address);
+        expect(result.transactions).toHaveTransaction({
+            from: shares.address,
+            to: keyAddress,
+            success: true
+        })
+
+        // key supply
+
+        const keyContract = blockchain.openContract(await SharesKey.fromAddress(keyAddress));
+        const keySupply = await keyContract.getSupply();
+        expect(keySupply).toEqual(4n);
+
+        // wallet balance
+        const walletAddress = await shares.getGetWalletAddress(holder.address, subject.address);
+        expect(result.transactions).toHaveTransaction({
+            from: shares.address,
+            to: walletAddress,
+            success: true
+        })
+
+        const walletContract = blockchain.openContract(await SharesWallet.fromAddress(walletAddress));
+
+        const walletBalance = await walletContract.getBalance();
+        expect(walletBalance).toEqual(1n);
 
     })
 
